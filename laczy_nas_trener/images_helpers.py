dbca8834feb348713e094ac433c6c77336400c91
Przemyslaw Kepka <przemyslaw.kepka@yahoo.com>
2023-01-23 18:50:12 +0100

    version v1.0 with many coaches and extra features

--- laczy_nas_trener/images_helpers.py
+++ laczy_nas_trener/images_helpers.py
@@ -1,14 +1,33 @@
 import base64
 from io import BytesIO
 from PIL import Image
+import streamlit as st
 
-def image_to_base64(img_path: str) -> str:
+def image_to_base64(img_path: str, logo_resize: bool = False) -> str:
     """
     Helper function returning base64 object stored as string,
         that can be displayed within the application.
     """
-    img = Image.open(img_path)
+    if logo_resize:
+        img = Image.open(img_path).resize((250, 250))
+    else:
+        img = Image.open(img_path)
     with BytesIO() as buffer:
         img.save(buffer, "png")  # or 'jpeg'
 
         return base64.b64encode(buffer.getvalue()).decode()
+
+def add_logo(image_base_64):
+    return st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(data:image/jpg;base64,%s);
+                background-repeat: no-repeat;
+                padding-top: 200px;
+                background-position: 20px 20px;
+            }
+        </style>
+        """ % image_base_64,
+        unsafe_allow_html=True,
+    )
---
+++ pages/📜_Info.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+from laczy_nas_trener import images_helpers
+
+image_base_log = images_helpers.image_to_base64('images/logo.jpg', logo_resize=True)
+images_helpers.add_logo(image_base_log)
+
+image_base_paulo = images_helpers.image_to_base64('images/paulo.jpg')
+#st.markdown(f'<div style="text-align: center;"> <img src="data:image/jpg;base64,{image_base_paulo}" width="150" height="275"> </div>',
+#            unsafe_allow_html=True)
+st.image('images/paulo.jpg', width=250)
+
+st.markdown('''Strona powstała wyłacznie w celach rozrywkowych, i nie ma na celu urażać innych osób.
+    \nLogo 'Męczy nas piłka' oraz ikony były trenerów to popularne w sieci grafiki pariodujące hasło 'Lączy nas piłka' (autor nieznany?).
+    \nObrazek na górze strony pochodzi z serwisu Wykop.pl, z tagu #reprezentacja (autor nieznany?).
+    \nGrafiki z wybranymi trenerami są oczywiście fikcyjne, część z nich pochodzi z profilu @PanieKazimierzu na portalu Twitter.com, dla
+        części z nich autor jest nieznany, natomiast większość z nich została stworzona przeze autora strony.
+''')
